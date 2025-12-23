a3c44407b0ccbbc862cfc86a99fa592704192918
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2025-12-23 15:19:36 +0100

    Starting up project

---
+++ app/src/main/java/com/matilfa/twentyquestions/MainActivity.java
@@ -0,0 +1,37 @@
+package com.matilfa.twentyquestions;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Locale locale;
+    private ResourceBundle resourceBundle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setup();
+    }
+
+    private void setup() {
+        locale = Locale.forLanguageTag("sv");
+        resourceBundle = ResourceBundle.getBundle("textResource", locale);
+    }
+}
