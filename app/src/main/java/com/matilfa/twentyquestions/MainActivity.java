934b868f41cce9191a7e95e242673095be977ed4
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2025-12-23 18:38:56 +0100

    Adding locale specific text to button

--- app/src/main/java/com/matilfa/twentyquestions/MainActivity.java
+++ app/src/main/java/com/matilfa/twentyquestions/MainActivity.java
@@ -1,6 +1,9 @@
 package com.matilfa.twentyquestions;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -18,20 +22,56 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+
             return insets;
         });
 
         setup();
     }
 
-    private void setup() {
-        locale = Locale.forLanguageTag("sv");
-        resourceBundle = ResourceBundle.getBundle("textResource", locale);
+    private void loadNewQuestion() {
+
     }
+
+    private void setup() {
+//        var locales = Locale.getAvailableLocales();
+//        for (Locale loc :
+//                locales) {
+////            Toast.makeText(MainActivity.this, loc.getCountry(), Toast.LENGTH_SHORT).show();
+////            System.out.println();
+//
+//        }
+//
+//        System.out.println(Arrays.stream(locales).anyMatch(loc ->
+//                loc.equals(Locale.forLanguageTag("sv"))));
+//
+//        var hasLocale = Arrays.stream(locales).anyMatch(loc ->
+//                loc.equals(Locale.forLanguageTag("sv")));
+//
+//        System.out.println(hasLocale);
+//
+//
+//        locale = Locale.forLanguageTag("sv");
+//        resourceBundle = ResourceBundle.getBundle("textResource", locale);
+
+        Button nextButton = findViewById(R.id.nextQuestionButton);
+
+        nextButton.setText(R.string.button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadNewQuestion();
+            }
+        });
+    }
+
+
 }
