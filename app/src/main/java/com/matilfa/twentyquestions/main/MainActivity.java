846f7cb5529749e97ea68f39f92768eef5a3cc08
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2025-12-28 22:05:02 +0100

    Cleaning up comments

--- app/src/main/java/com/matilfa/twentyquestions/main/MainActivity.java
+++ app/src/main/java/com/matilfa/twentyquestions/main/MainActivity.java
@@ -49,9 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
                 v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
-//                populateQuestionsList();
 
-//                setup();
                 questionRepository = new QuestionRepository(
                         this, findViewById(R.id.questionText));
                 questionRepository.setup();
@@ -64,10 +62,6 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         questionRepository.generateRandomQuestion();
 
-//                        String question = generateRandomQuestion();
-//
-//                        TextView questionTextLabel = findViewById(R.id.questionText);
-//                        questionTextLabel.setText(question);
                     }
                 });
 
@@ -103,80 +97,13 @@ public class MainActivity extends AppCompatActivity {
             questions.add(question);
         }
     }
-//
-//    private void copyDatabaseFile() throws IOException {
-//
-//
-
-    /// /        try (InputStream is = this.getResources().openRawResource())
-    /// /
-    /// /        try (FileInputStream fis = this.openFileInput("twentyQuestions.db");
-    /// /             BufferedInputStream bis = new BufferedInputStream(fis)) {
-    /// /            FileOutputStream fos = this.openFileOutput("twentyQuestions.db", this.MODE_PRIVATE);
-    /// /
-    /// /            bis.transferTo(fos)
-    /// /        }
-//
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-//            if (!Files.isDirectory(Path.of(getPackageName() + "/data/data"))) {
-//                Files.createDirectory(Path.of(getPackageName() + "/data/data"));
-//            }
-//
-//            Files.copy(Path.of("com/matilfa/twentyquestions/data/twentyQuestions.db"),
-//                    Path.of(getPackageName() + "/data/data"));
-//
-//        } else {
-//            Toast.makeText(
-//                            this,
-//                            "Has no support for old Android versions yet.",
-//                            Toast.LENGTH_SHORT)
-//                    .show();
-//            throw new IOException("Cannot create or access data directory due to unsupported Android version.");
-//        }
-//    }
-//    private void setup() {
-//
-//        db = Room.databaseBuilder(getApplicationContext(),
-//                QuestionDatabase.class, "twentyQuestions.db").build();
-//
-//        questionDao = db.questionDao();
-//
-//        ExecutorService executors = Executors.newSingleThreadExecutor();
-//        try {
-//             executors.submit(() ->
-//                     questionDao.insertAll(questions)
-//             );
-//        }
-//        finally {
-//            executors.shutdown();
-//        }
-//
-////        var all = questionDao.getAll();
-//
-//
-//    }
 
     private String generateRandomQuestion() {
         int randomNo = ThreadLocalRandom
                 .current()
                 .nextInt(0, allQuestions.size());
 
-
-//        return getById(1);
         return allQuestions.get(randomNo);
-//        final Question question;
-//        ExecutorService executors = Executors.newSingleThreadExecutor();
-//        try {
-//            executors.submit(() ->
-//                    questionDao.getById(randomNo)
-//            );
-//        }
-//        finally {
-//            executors.shutdown();
-//        }
-
     }
-
 
 }
