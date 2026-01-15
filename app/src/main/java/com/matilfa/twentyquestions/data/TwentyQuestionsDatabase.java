75bed358c59411346239bfd20f0308a410e14a11
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-01-15 21:53:45 +0100

    Rearranging thread work in TwentyQuestionsDatabase.java

--- app/src/main/java/com/matilfa/twentyquestions/data/TwentyQuestionsDatabase.java
+++ app/src/main/java/com/matilfa/twentyquestions/data/TwentyQuestionsDatabase.java
@@ -13,14 +13,23 @@ import com.matilfa.twentyquestions.data.sessions.SessionDao;
 import com.matilfa.twentyquestions.data.users.User;
 import com.matilfa.twentyquestions.data.users.UserDao;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Database(entities = {Question.class, Session.class, User.class}, version = 1, exportSchema = false)
+//todo: fix data migration
 public abstract class TwentyQuestionsDatabase extends RoomDatabase {
 
     private static volatile TwentyQuestionsDatabase dbInstance;
     public static final String DATABASE_NAME = "twentyQuestions.db";
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
     public abstract QuestionDao questionDao();
+
     public abstract SessionDao sessionDao();
+
     public abstract UserDao userDao();
 
     public static TwentyQuestionsDatabase getInstance(final Context context) {
@@ -35,3 +44,4 @@ public abstract class TwentyQuestionsDatabase extends RoomDatabase {
         return dbInstance;
     }
 }
+
