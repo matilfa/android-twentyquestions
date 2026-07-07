d7f9ad47926922723e4322fa813ec139d7d6bb08
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-07-08 00:05:30 +0200

    Adding Hilt and KSP dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,103 +1,108 @@
-    plugins {
-        alias(libs.plugins.android.application)
-        kotlin("plugin.serialization") version "2.0.21"
-        id("androidx.navigation.safeargs")
-//        id("com.google.devtools.ksp")
-//        id("com.google.dagger.hilt.android")
+plugins {
+    alias(libs.plugins.android.application)
+    id("org.jetbrains.kotlin.android")
+    kotlin("plugin.serialization") version "2.0.21"
+    id("androidx.navigation.safeargs")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+
+}
+
+android {
+    namespace = "com.matilfa.twentyquestions"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.matilfa.twentyquestions"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
 
     }
 
-    android {
-        namespace = "com.matilfa.twentyquestions"
-        compileSdk = 36
-
-        defaultConfig {
-            applicationId = "com.matilfa.twentyquestions"
-            minSdk = 26
-            targetSdk = 36
-            versionCode = 1
-            versionName = "1.0"
-
-            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        }
-
-        buildTypes {
-            release {
-                isMinifyEnabled = false
-                proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
-                )
-            }
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
-
-        }
-
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
-    dependencies {
+}
 
-        implementation(libs.appcompat)
-        implementation(libs.material)
-        implementation(libs.activity)
-        implementation(libs.constraintlayout)
-        implementation(libs.recyclerview)
-        testImplementation(libs.junit)
-        androidTestImplementation(libs.ext.junit)
-        androidTestImplementation(libs.espresso.core)
+dependencies {
 
-        implementation(platform("androidx.compose:compose-bom:2025.12.01"))
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 
-        //For database:
-        val room_version = "2.8.4"
+    implementation(platform("androidx.compose:compose-bom:2025.12.01"))
 
-        implementation("androidx.room:room-runtime:$room_version")
-        annotationProcessor("androidx.room:room-compiler:$room_version")
+    //For database:
+    val room_version = "2.8.4"
 
-        //For fragments
-        val fragment_version = "1.8.9"
-        implementation("androidx.fragment:fragment:${fragment_version}")
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 
-        //For navigation
-        val nav_version = "2.9.8"
+    //For fragments
+    val fragment_version = "1.8.9"
+    implementation("androidx.fragment:fragment:${fragment_version}")
 
-        implementation("androidx.navigation:navigation-fragment:$nav_version")
-        implementation("androidx.navigation:navigation-ui:$nav_version")
+    //For navigation
+    val nav_version = "2.9.8"
 
-        // Feature module support for Fragments
-        implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
 
-        // Testing Navigation
-        androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+    // Feature module support for Fragments
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
 
-        // JSON serialization library, works with the Kotlin serialization plugin
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
 
-        //Hilt
-//        implementation("com.google.dagger:hilt-android:2.57.1")
-//        ksp("com.google.dagger:hilt-android-compiler:2.57.1")
+    // JSON serialization library, works with the Kotlin serialization plugin
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
-        // ViewModel
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
 
-        val lifecycle_version = "2.11.0"
-        val arch_version = "2.2.0"
+    // ViewModel
 
-        // ViewModel
-        implementation("androidx.lifecycle:lifecycle-viewmodel:$lifecycle_version")
-        // LiveData
-        implementation("androidx.lifecycle:lifecycle-livedata:$lifecycle_version")
-        // Saved state module for ViewModel
-        implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:${lifecycle_version}")
-        // Annotation processor
-        annotationProcessor("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+    val lifecycle_version = "2.11.0"
+    val arch_version = "2.2.0"
 
-        // optional - Test helpers for LiveData
-        testImplementation("androidx.arch.core:core-testing:$arch_version")
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata:$lifecycle_version")
+    // Saved state module for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:${lifecycle_version}")
+    // Annotation processor
+    annotationProcessor("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
 
-        // optional - Test helpers for Lifecycle runtime
-        testImplementation("androidx.lifecycle:lifecycle-runtime-testing:$lifecycle_version")
+    // optional - Test helpers for LiveData
+    testImplementation("androidx.arch.core:core-testing:$arch_version")
 
-    }
+    // optional - Test helpers for Lifecycle runtime
+    testImplementation("androidx.lifecycle:lifecycle-runtime-testing:$lifecycle_version")
+
+}
