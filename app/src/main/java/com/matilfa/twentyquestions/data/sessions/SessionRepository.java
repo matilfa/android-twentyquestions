46f9bb539369a333bcb67ed5f3fbdd3aaf463f30
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-07-17 22:21:06 +0200

    Implementing a createSession method

--- app/src/main/java/com/matilfa/twentyquestions/data/sessions/SessionRepository.java
+++ app/src/main/java/com/matilfa/twentyquestions/data/sessions/SessionRepository.java
@@ -3,6 +3,12 @@ package com.matilfa.twentyquestions.data.sessions;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.room.Transaction;
+
+import com.matilfa.twentyquestions.data.users.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -22,5 +28,33 @@ public class SessionRepository {
 
     public Session getSessionByName(@NonNull String sessionName) {
         return sessionDao.getByName(sessionName);
+    }
+
+    /**
+     * Inserts a new session into the database, and insert entries into the junction table for each
+     * user in the list.
+     * This is done within a transaction, which will rollback in case of some failure in the process.
+     * @param sessionName
+     * @param usersInSession
+     * @return
+     */
+    @Transaction
+    public Session createSession(String sessionName, List<User> usersInSession) {
+        Session session = new Session();
+        session.name = sessionName;
+        session.sessionId = sessionDao.insertSession(session);
+
+        List<UserSessionCrossRef> crossRefs = new ArrayList<>();
+        for (User user :
+                usersInSession) {
+            var cr = new UserSessionCrossRef();
+            cr.sessionId = session.sessionId;
+            cr.userId = user.userId;
+
+            crossRefs.add(cr);
+        }
+        sessionDao.insertAllUserCrossRefs(crossRefs);
+
+        return session;
     }
 }
